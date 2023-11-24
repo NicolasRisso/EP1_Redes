7d084bcbc3db4c46eeba51494e7e37d228002b15
Nicolas Risso <risso.nicolas@usp.br>
2023-11-24 15:10:51 -0300

    Chat - Username

--- code/client.py
+++ code/client.py
@@ -1,20 +1,30 @@
 import socket
+import struct
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 9999))
 
+username = input("Username: ")
+
 done = False
 while not done:
-    input_var = input("Mensagem: ")
-    response = input_var.encode('utf-8')
-    if input_var == "QUIT":
+    #Pegue a mensagem do usuário
+    message = input(f"({username}): ")
+
+    #Mande o tamanho do username e da mensagem, depois envia a mensagem / username em si
+    client.send(struct.pack('!I', len(username)))
+    client.send(username.encode('utf-8'))
+
+    client.send(struct.pack('!I', len(message)))
+    client.send(message.encode('utf-8'))
+
+    if message == "QUIT":
         done = True
-
-    client.send(response)
-
-    if done: break
-
-    msg = client.recv(1024).decode('utf-8')
-    print(msg)
+    else:
+        response_username_lenght =struct.unpack('!I', client.recv(4))[0]
+        response_username = client.recv(response_username_lenght).decode('utf-8')
+        response_length = struct.unpack('!I', client.recv(4))[0]
+        response = client.recv(response_length).decode('utf-8')
+        print(f"({response_username}): {response}")
 
 client.close()
--- code/server.py
+++ code/server.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("localhost", 9999))
@@ -6,18 +7,31 @@ server.listen()
 
 client, address = server.accept()
 
+username = input("Username: ")
+
 done = False
-
 while not done:
-    msg = client.recv(1024).decode('utf-8')
-    print(msg)
+    #Recebe o tamanho do username e depois o username
+    response_username_length = struct.unpack('!I', client.recv(4))[0]
+    response_username = client.recv(response_username_length).decode('utf-8')
 
-    if msg == "QUIT": 
+    #Recebe o tamanho da mensagem e depois a mensagem
+    message_length = struct.unpack('!I', client.recv(4))[0]
+    message = client.recv(message_length).decode('utf-8')
+
+    print(f"({response_username}): {message}")
+
+    if message == "QUIT":
         done = True
-        break
+    else:
+        # Respond to the client
+        response = input(f"({username}): ")
+        
+        # Send the length of the response and the response itself
+        client.send(struct.pack('!I', len(username)))
+        client.send(username.encode('utf-8'))
 
-    input_var = input("Mensagem: ")
-    response = input_var.encode('utf-8')
-    client.send(response)
+        client.send(struct.pack('!I', len(response)))
+        client.send(response.encode('utf-8'))
 
 client.close()
