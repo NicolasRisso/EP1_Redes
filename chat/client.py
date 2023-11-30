ebf5d5f1d8d3f71b4c48abcf59c0b2e478d2ea0b
Nicolas Risso <risso.nicolas@usp.br>
2023-11-30 13:30:15 -0300

    Chat C-S: p2
    >Comentado por completo.
    >O terminal congela enquanto espera a resposta do usuário, precisa ser arrumado.

--- chat/client.py
+++ chat/client.py
@@ -1,6 +1,8 @@
 import socket
 import select
 import errno
+import sys
+import threading
 
 HEADER_LENGTH = 10
 
@@ -8,75 +10,70 @@ IP = "localhost"
 PORT = 6969
 my_username = input("Username: ")
 
-# Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+#Cria um Socket TCP para se comunicar com o servidor
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to a given ip and port
+#Conecta
 client_socket.connect((IP, PORT))
 
-# Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
+#Define a conexão para um estado de "não bloqueamento", assim, o código não ficará travado
+#enquanto espera uma resposta do servidor.
 client_socket.setblocking(False)
 
-# Prepare username and header and send them
-# We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
+#Prepara o header e a mensagem contendo o Username e os envia.
 username = my_username.encode('utf-8')
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
 
 while True:
 
-    # Wait for user to input a message
-    message = input(f'{my_username} > ')
+    #PROBLEMA AQUI, ISSO NÃO DEIXA DINAMICO A RECEPÇÃO
+    #Espera até o usuário digitar sua mensagem
+    message = input(f'{my_username}: ')
 
-    # If message is not empty - send it
+    #Se a mensagem não for vazio, a envia.
     if message:
 
-        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+        #Encoda a mensagem em bytes e a envia junto com o header.
         message = message.encode('utf-8')
         message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
         client_socket.send(message_header + message)
 
     try:
-        # Now we want to loop over received messages (there might be more than one) and print them
+        #loop entre todas as mensagens recebidas nesse meio tempo.
         while True:
 
-            # Receive our "header" containing username length, it's size is defined and constant
+            #Header
             username_header = client_socket.recv(HEADER_LENGTH)
 
-            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+            #Servidor fechou a conexão
             if not len(username_header):
-                print('Connection closed by the server')
+                print('Conexão desligada pelo Servidor')
                 sys.exit()
 
-            # Convert header to int value
+            #Header -> int
             username_length = int(username_header.decode('utf-8').strip())
 
-            # Receive and decode username
+            #Decodifica o username
             username = client_socket.recv(username_length).decode('utf-8')
 
-            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+            #Decodifica a mensagem (com header)
             message_header = client_socket.recv(HEADER_LENGTH)
             message_length = int(message_header.decode('utf-8').strip())
             message = client_socket.recv(message_length).decode('utf-8')
 
-            # Print message
-            print(f'{username} > {message}')
+            print(f'{username}: {message}')
 
     except IOError as e:
-        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
-        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
-        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
-        # If we got different error code - something happened
+        #Detectar possíveis erros do sistema.
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
             print('Reading error: {}'.format(str(e)))
             sys.exit()
 
-        # We just did not receive anything
+        #Se chegar aqui, não tem erro.
         continue
 
     except Exception as e:
-        # Any other exception - something happened, exit
+        #Qualquer outra exception, como um exit.
         print('Reading error: '.format(str(e)))
         sys.exit()
