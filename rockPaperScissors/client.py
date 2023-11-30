2b343951b1faa12d90f418dffe697907c3cdf0a0
Nicolas Risso <risso.nicolas@usp.br>
2023-11-30 15:54:13 -0300

    Organização
    >Código do PPT ta zoneado p krl, mas dei uma limpada

--- rockPaperScissors/client.py
+++ rockPaperScissors/client.py
@@ -17,36 +17,12 @@ button_images = [pygame.image.load("../art/icons/pedra.png"),
 background_image = pygame.image.load("../art/icons/moldura.png")
 
 
-class Button:
-    def __init__(self, text, x, y, color):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.color = color
-        self.width = 150
-        self.height = 100
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        font = pygame.font.SysFont("roboto", 40)
-        text = font.render(self.text, 1, (255,255,255))
-        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
-
-    def click(self, pos):
-        x1 = pos[0]
-        y1 = pos[1]
-        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
-            return True
-        else:
-            return False
-
-
 def redrawWindow(win, game, p):
-    win.fill((128,128,128))
+    win.fill((200,200,200))
 
     if not(game.connected()):
         font = pygame.font.SysFont("roboto", 60)
-        text = font.render("Esperando por outro Jogador...", 1, (255,0,0), True)
+        text = font.render("Esperando por outro Jogador...", 1, (255,0,0))
         win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
     else:
         font = pygame.font.SysFont("roboto", 45)
